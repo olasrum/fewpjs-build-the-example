fddbdad2d5dff6bea27faafbb03d117a4ced01b0
olasrum <mursalo@gmail.com>
2022-08-09 17:34:12 +1000

    Done with assignment

--- main.js
+++ main.js
@@ -4,8 +4,42 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const modal = document.getElementById('modal')
 
+function hideModal() {
+  modal.setAttribute('class', 'hidden')
+}
 
+function clickHeart(event) {
+  let heart = event.target
+  if (heart.innerHTML === EMPTY_HEART) {
+    mimicServerCall()
+    .then(server => {
+      heart.innerHTML = FULL_HEART
+      heart.setAttribute('class', 'activated-heart')
+    })
+    .catch((reason) => {
+      modal.removeAttribute('class', 'hidden')
+      modal.innerHTML = reason
+      window.setTimeout(hideModal, 5000)
+    })
+  } else {
+    heart.innerHTML = EMPTY_HEART
+    heart.removeAttribute('class', 'activated-heart')
+  }
+}
+
+hideModal()
+
+document.addEventListener('DOMContentLoaded', () => {
+  const likes = document.getElementsByClassName('like-glyph')
+  for (const heart in likes) {
+    if (likes.hasOwnProperty(heart)) {
+      const element = likes[heart]
+      element.addEventListener('click', clickHeart)
+    }
+  }
+})
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
